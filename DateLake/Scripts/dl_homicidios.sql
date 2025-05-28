d864dec88715d32521843e6808397755236b7887
primera prueba git <gaalrohe@gmail.com>
2025-05-28 11:51:21 -0500

    Update dl_homicidios.sql
    corrige codigo

--- DateLake/Scripts/dl_homicidios.sql
+++ DateLake/Scripts/dl_homicidios.sql
@@ -4,16 +4,13 @@
 
 -- DROP TABLE DateWareHouse.dbo.fac_homicidios;
 
-CREATE TABLE DateWareHouse.dbo.fac_homicidios (
-	IdHomicidios int IDENTITY(1,1) NOT NULL,
-	FechaHecho date NOT NULL,
-	CodMpio int NOT NULL,
-	IdZona tinyint NOT NULL,
-	IdSexo tinyint NOT NULL,
-	Cantidad tinyint NOT NULL,
-	CONSTRAINT fac_homicidios_pk PRIMARY KEY (IdHomicidios),
-	CONSTRAINT fac_homicidios_dim_fechas_FK FOREIGN KEY (FechaHecho) REFERENCES DateWareHouse.dbo.dim_fechas(FechaHecho),
-	CONSTRAINT fac_homicidios_dim_municipios_FK FOREIGN KEY (CodMpio) REFERENCES DateWareHouse.dbo.dim_municipios(CodMpio),
-	CONSTRAINT fac_homicidios_dim_zonas_FK FOREIGN KEY (IdZona) REFERENCES DateWareHouse.dbo.dim_zonas(IdZona),
-	CONSTRAINT fac_homicidios_din_sexo_FK FOREIGN KEY (IdSexo) REFERENCES DateWareHouse.dbo.din_sexo(IdSexo)
+CREATE TABLE DataLake.dbo.dl_homicidios (
+	[FECHA HECHO] date NULL,
+	COD_DEPTO tinyint NULL,
+	DEPARTAMENTO varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	COD_MUNI tinyint NULL,
+	MUNICIPIO varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	ZONA varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	SEXO varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CANTIDAD tinyint NULL
 );
